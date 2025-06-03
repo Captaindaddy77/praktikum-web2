89ad8c40eba070ef29ad8a3ac4c970b9bdbff37f
Captaindaddy77 <nabilzaky77@gmail.com>
2025-06-03 09:21:06 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/nabil.jpg" alt="Nabil Zaky" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2310010511</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Nabil Zaky</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Buntok, 19 Februari 2005</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0852 1073 3992</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">nabilzaky77@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.facebook.com/share/1Zd6u4FSVS/?mibextid=wwXIfr" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/nabilllzzz" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://steamcommunity.com/profiles/76561199576437218/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/steam.svg" width="30" alt="steam">
+                            </a>
+                            <a href="https://discord.com/users/harrypotter77" target="_blank">
+                                <img src="img/medsos/discord.svg" width="30" alt="discord">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
